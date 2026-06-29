2cddb9b14faea17fde86871ca25b59dd0bc6023b
masoud dabbaghi <masouddabbaghi0@gmail.com>
2026-06-29 11:17:36 +0330

    add vercel.json for deploy

--- src/layout/ProjectLayout.jsx
+++ src/layout/ProjectLayout.jsx
@@ -8,7 +8,7 @@ const ProjectLayout = ({ children }) => {
     <div className="flex justify-around items-center">
       <Link className="flex gap-x-10 items-center text-indigo-700 p-2 rounded-lg" to="/projects">
         <FaArrowLeft className="scale-150 cursor-pointer" />
-        <Button>Back to Projects </Button>
+        <Button>Back to Projects</Button>
       </Link>
       {children}
     </div>
