71abf65a5aeabd9e4e32f4f604a213c12b0227ca
masoud dabbaghi <masouddabbaghi0@gmail.com>
2026-06-29 13:15:06 +0330

    add info

--- src/layout/ProjectLayout.jsx
+++ src/layout/ProjectLayout.jsx
@@ -3,14 +3,20 @@ import { FaArrowLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Button from "../components/Button";
 
-const ProjectLayout = ({ children }) => {
+const ProjectLayout = ({ children , text }) => {
   return (
     <div className="flex justify-around items-center">
-      <Link className="flex gap-x-10 items-center text-indigo-700 p-2 rounded-lg" to="/projects">
-        <FaArrowLeft className="scale-150 cursor-pointer" />
-        <Button>Back to Projects</Button>
-      </Link>
-      {children}
+      <div className="flex-col items-center justify-center">
+        <Link
+          className="flex gap-x-10 items-center text-indigo-700 p-2 rounded-lg"
+          to="/projects"
+        >
+          <FaArrowLeft className="scale-150 cursor-pointer" />
+          <Button>Back to Projects</Button>
+        </Link>
+          <p className="font-bold bg-gradient-to-l from-indigo-950 to-indigo-700 text-white p-4 rounded-3xl ">{text}</p>
+      </div>
+      <div className="">{children}</div>
     </div>
   );
 };
--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ProjectLayout from '../layout/ProjectLayout'
 import BankCardPayment from '../components/BankCardPayment'
 
-const Payment = () => {
+const Payment = ({text}) => {
+
   return (
-    <ProjectLayout>
+    <ProjectLayout text={'شش رقم اول کارت بانکی, مبدا بانک شما رو مشخص میکنه'} className="">
         <BankCardPayment />
     </ProjectLayout>
   )
